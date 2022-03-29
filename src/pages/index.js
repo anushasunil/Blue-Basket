1d133aa8f8b174a7a9f456911d94b518af28c0ac
Anusha <anushasunil71201@gmail.com>
2022-03-29 23:33:53 +0530

    imported `<SignUpPage/>`

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,10 @@ import { Nav } from "../components/Nav/Nav";
 import { Cart } from "../pages/Cart/Cart";
 import { Wishlist } from "../pages/Wishlist/Wishlist";
 import { LoginPage } from "./Authentication/Login/Login";
+import { SignUpPage } from "./Authentication/SignUp/SignUp";
 import { ErrorPage } from "../pages/Error/ErrorPage";
 import { Footer } from "../components/Footer/Footer";
 import { ProductListing } from "./ProductListing/ProductListing";
 import { ProductListingProvider } from "../contexts/product-context";
 
-export {Home, MockbeeHome, MockmanComponent, Nav, Cart, Wishlist, LoginPage, ErrorPage, Footer, ProductListing, ProductListingProvider}
+export {Home, MockbeeHome, MockmanComponent, Nav, Cart, Wishlist, LoginPage, ErrorPage, Footer, ProductListing, ProductListingProvider, SignUpPage}
