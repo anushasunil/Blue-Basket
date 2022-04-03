db8378aa2ce5bceba658ff12eb22b0fd2df43015
Anusha <anushasunil71201@gmail.com>
2022-04-03 19:12:38 +0530

    created `useAxios` hook
    created for both GET and POST methods

--- src/utility/hooks/useAxios.js
+++ src/utility/hooks/useAxios.js
@@ -1,19 +1,32 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const useAxios = (endpoint) => {
-
+const useAxios = (...args) => {
+    const [method, endpoint, content] = [...args];
     const [loading, setLoading] = useState(true);
-    const [responseData, setResponseData] = useState([]);
+    const [responseData, setResponseData] = useState({});
     const [errorFlag, setErrorFlag] = useState(false);
 
     useEffect(() => {
 
         (async () => {
             try {
-                setLoading(true);
-                const response = await axios.get(endpoint);
-                setResponseData(response.data);
+                const response = {}
+                switch(method){
+                    case "GET" : 
+                        setLoading(true);
+                        response = await axios.get(endpoint).then(response => setResponseData(response.data));
+                        break;
+                    case "POST": 
+                        setLoading(true);
+                        console.log("evejbvhbrjvb entered post function");
+                        response = await axios.post(endpoint, content).then(response => {
+                            setResponseData(response.data); 
+                            console.log(response)
+                        });
+                        break;
+                    default: console.log("error but okay")
+                }
             } catch (error) {
                 setErrorFlag(true);
             } finally {
